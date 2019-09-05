a190f5aeec9f472970de73cb71eb714818ec76ff
decent-token-6050 <garrettburns@mac.com>
2019-09-05 19:40:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+
 def square_array(array)
-  # your code here
+  @numbers = [1, 2, 3]
+  while @numbers.size > 0
+    square_this = @numbers.shift
+    squared = square_this ** 2
+    puts squared
+    @numbers.push(squared)
+    #p @numbers
+  end
+  
 end
